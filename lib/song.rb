96e60d8302471b1a4fab21e6423a84f659b5aebc
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 01:12:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,15 @@
 class Song
-attr_accessor 
+attr_accessor :songs, :artists, :genres
 @@songs = 0
 @@artists = []
 @@genres = []
 @@genre_count = {}
 @@artist_count = {}
 
+def initialize
+  @@songs += 1
 
+end
 
 
 
