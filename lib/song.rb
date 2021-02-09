137abe4ab08deb5d8674aad2dd8f4173a4773761
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 01:24:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,20 +10,20 @@ def new(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
-  :@@count += 1
-  :@@genres << genre
-  :@@artists << artist
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
 end
 
 def count
-  :@@count
+  @@count
 end
 
 def genres
-  :@@genres.uniq!
+  @@genres.uniq!
 end
 
 def artists
-  :@@artists.uniq!
+  @@artists.uniq!
 end
 end
